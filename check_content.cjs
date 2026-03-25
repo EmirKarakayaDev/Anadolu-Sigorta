17469e3b3ab0db5b51f5988f2011922dacd0b838
Emir Karakaya <dev.emirkarakaya@gmail.com>
2026-03-25 13:23:43 +0300

    project cleanup and asset optimization

--- check_content.cjs
+++
@@ -1,3 +0,0 @@
-const fs = require('fs');
-const content = fs.readFileSync('kvkk_v3.txt', 'utf-8');
-console.log(content);
--- temp_pdf/debug_pdf.cjs
+++
@@ -1,4 +0,0 @@
-const pdfModule = require('pdf-parse');
-console.log('Keys:', Object.keys(pdfModule));
-if (pdfModule.pdf) console.log('pdf is a function');
-if (pdfModule.default) console.log('default is a function', typeof pdfModule.default);
